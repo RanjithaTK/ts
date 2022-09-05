21eb98b6acb9640cda1fd97a4d92bda1f7b25f49
im ranjitha <ranjithatk101@gmail.com>
2022-09-05 23:21:21 +0530

    added ts

---
+++ hello/oops.ts
@@ -0,0 +1,17 @@
+//ENcapsulation
+class A {
+  a: string
+  b: string
+  c: string
+  d: string
+
+  constructor(a: string, b: string, c: string, d: string) {
+    this.a = a
+    this.b = b
+    this.c = c
+    this.d = d
+  }
+  print(): string {
+    return `${this.a} - ${this.b} -${this.c} -${this.d}`
+  }
+}
